84c2e2d353edeeb9dda02a5ec6e13b8ef0fdd3ab
Your Name <you@example.com>
2025-05-27 04:10:18 -0500

    lo

--- scripts/products.js
+++ scripts/products.js
@@ -4,7 +4,7 @@ const products = [
         id: 1,
         name: "Picante de Cuy 1/2",
         category: "comidas",
-        price: 35.00,
+        price: 45.00,
         description: "Delicioso cuy al horno acompañado de papas doradas y salsa de maní.",
         image: "./images/menu/picante-de-cuy.jpg",
         available: true
@@ -13,7 +13,7 @@ const products = [
         id: 2,
         name: "Chicharrón de Chancho",
         category: "comidas",
-        price: 28.00,
+        price: 35.00,
         description: "Crujiente chicharrón de cerdo servido con camote frito, mote y salsa criolla.",
         image: "./images/menu/chicharron-de-chancho.jpg",
         available: true
@@ -22,7 +22,7 @@ const products = [
         id: 3,
         name: "Frejolada de Pato",
         category: "comidas",
-        price: 32.00,
+        price: 35.00,
         description: "Tradicional frejolada preparada con pato tierno y especias.",
         image: "./images/menu/frejolada-de-pato.jpg",
         available: true
@@ -31,7 +31,7 @@ const products = [
         id: 4,
         name: "Cabrito Norteno",
         category: "comidas",
-        price: 38.00,
+        price: 35.00,
         description: "Exquisito cabrito preparado al estilo norteño con frijoles y yuca.",
         image: "./images/menu/cabrito-norteno.jpg",
         available: false
@@ -40,7 +40,7 @@ const products = [
         id: 5,
         name: "Costillar",
         category: "comidas",
-        price: 40.00,
+        price: 35.00,
         description: "Costillar de cerdo glaseado con salsa barbacoa, acompañado de puré de papas.",
         image: "./images/menu/costillar.jpg",
         available: true
@@ -49,49 +49,40 @@ const products = [
         id: 6,
         name: "Pepian Pavita de Corral",
         category: "comidas",
-        price: 30.00,
+        price: 35.00,
         description: "Pavita criolla en salsa de pepian con arroz y papas.",
         image: "./images/menu/pepian-pavita.jpg",
         available: true
     },
     {
         id: 7,
-        name: "Chancho a la Caja China",
-        category: "comidas",
-        price: 45.00,
-        description: "Lechón asado a la caja china, servido con tamalitos verdes y salsa criolla.",
-        image: "./images/menu/chancho-caja-china.jpg",
-        available: true
-    },
-    {
-        id: 8,
         name: "Caldo de Gallina",
         category: "comidas",
-        price: 22.00,
+        price: 25.00,
         description: "Reconfortante caldo preparado con gallina criolla, fideos y hierbas aromáticas.",
         image: "./images/menu/caldo-gallina.jpg",
         available: true
     },
     {
-        id: 9,
+        id: 8,
         name: "Ceviche",
         category: "comidas",
-        price: 25.00,
+        price: 35.00,
         description: "Fresco ceviche de pescado con leche de tigre, camote y cancha serrana.",
         image: "./images/menu/ceviche.jpg",
         available: false
     },
     {
-        id: 10,
+        id: 9,
         name: "Chicharrón de Pescado",
         category: "comidas",
-        price: 26.00,
+        price: 35.00,
         description: "Filete de pescado empanizado y frito, servido con yuca y salsa tartara.",
         image: "./images/menu/chicharron-pescado.jpg",
         available: true
     },
     {
-        id: 11,
+        id: 100,
         name: "Gaseosas 1L",
         category: "bebidas",
         price: 8.00,
@@ -100,7 +91,7 @@ const products = [
         available: true
     },
     {
-        id: 12,
+        id: 101,
         name: "Gaseosa 1/2L",
         category: "bebidas",
         price: 5.00,
@@ -109,7 +100,7 @@ const products = [
         available: true
     },
     {
-        id: 13,
+        id: 102,
         name: "Cervezas",
         category: "bebidas",
         price: 10.00,
@@ -118,7 +109,7 @@ const products = [
         available: true
     },
     {
-        id: 14,
+        id: 103,
         name: "Agua Mineral",
         category: "bebidas",
         price: 4.00,
